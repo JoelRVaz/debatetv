765b7950d873d24dbbffc99624efbe8f69050bfe
joelvaz <jrv1271663@gmail.com>
2021-03-08 18:52:43 -0800

    node_manager

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import ThumbnailCard from './components/ThumbnailCard/ThumbnailCard.js'
-import NavBar from './components/NavBar/NavBar.js';
-import ReactDOM from 'react-dom';
-
-
-
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <NavBar></NavBar>
-      <ThumbnailCard></ThumbnailCard>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
